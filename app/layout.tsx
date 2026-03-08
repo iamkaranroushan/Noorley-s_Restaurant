4d38d27a9055dc6219d502c6d83be91ae5ccec6d
Karan Roushan <karan.122roushan@gmail.com>
2026-03-09 00:54:52 +0530

    metadata update

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,9 +13,29 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+// app/layout.tsx (or RootLayout)
 export const metadata: Metadata = {
-  title: "Norley's Authentic Columbian Street food",
+  title: "Norley's Authentic Columbian Street Food",
   description: "Columbian Restaurant",
+  openGraph: {
+    type: "website",
+    url: "https://norley.karanroushan.com",
+    title: "Norley's Authentic Columbian Street Food",
+    description: "Order the best meals from Norley's, fast and fresh!",
+    images: [
+      {
+        url: "/image/sitelogo.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Norley's – Delicious Food Delivered",
+    description: "Order the best meals from Norley's, fast and fresh!",
+    images: ["/image/sitelogo.png"],
+  },
 };
 
 export default function RootLayout({
